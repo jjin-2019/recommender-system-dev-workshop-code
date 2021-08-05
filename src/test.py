37d1c34bc10be883f6d5648211c17a18a6e2c659
jjin-2019 <lijin2019@hotmail.com>
2021-08-05 18:03:59 +0800

    fix bugs

---
+++ src/test.py
@@ -0,0 +1,31 @@
+import json
+import pickle
+
+
+if __name__ == "__main__":
+    file = "/Users/jilizm/recommender-system-dev-workshop-code/sample-data/notification/inverted-list/recall_config_v1.pickle"
+    infile = open(file, 'rb')
+    dict = pickle.load(infile)
+    infile.close()
+    print(json.dumps(dict, indent= 2))
+    #
+    # json_file ={
+    #         "DatasetGroupName": "GCR-RS-News-Dataset-Group",
+    #         "UserDatasetName": "NewsUserDataset",
+    #         "ItemDatasetName": "NewsItemDataset",
+    #         "InteractionDatasetName": "NewsInteractionDataset",
+    #         "EventTrackerName": "NewsEventTracker",
+    #         "SolutionName": "userPersonalizeSolution",
+    #         "SolutionVersionArn": "arn:aws:personalize:ap-northeast-1:466154167985:solution/userPersonalizeSolutionNew/5286e329",
+    #         "CampaignName": "gcr-rs-dev-workshop-news-UserPersonalize-campaign",
+    #         "UserFileName": "personalize_user.csv",
+    #         "ItemFileName": "personalize_item.csv",
+    #         "InteractionFileName": "personalize_interaction.csv",
+    #         "TrainingMode": "UPDATE"
+    # }
+    # print(json_file.__str__())
+    # string = json.dumps(json_file)
+    # print(str(string))
+    # test = json.loads(string)
+    # print(test)
+    # print(test['SolutionVersionArn'])
